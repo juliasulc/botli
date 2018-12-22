a839a8e31d531ab5cb8235707d8a2a74af7fc26f
Julia <julia@manels-macbook-air.home>
2018-12-22 18:47:38 +0100

    cheesli friends

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const ccxt = require('ccxt')
+const bitfinex = new ccxt.bitfinex();
+
+console.log(ccxt);
+console.log(bitfinex);
+
 class App extends Component {
   render() {
     return (
