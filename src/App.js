f5474aa9a1f3edea16b77e77bc80432174d01558
Julia <julia@Manels-MacBook-Air.local>
2018-12-27 18:53:22 +0100

    trying a worker.2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import debugout from './debugout.js';
-import counter from '../worker/index';
+import counter from './worker/index';
 
 const bugout = new debugout();
 
