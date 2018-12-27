f27c8b5554878b46e54dfac81339ea5f3ac09ca6
Julia <julia@Manels-MacBook-Air.local>
2018-12-27 18:57:26 +0100

    trying a worker.3

---
+++ src/worker/index.js
@@ -0,0 +1,27 @@
+const ccxt = require('ccxt');
+const EmailService = require('./EmailService');
+
+const bitfinex = new ccxt.bitfinex();
+
+const emailService = new EmailService('gmail',{
+    user: 'nodelibotli@gmail.com',
+    pass: 'nodeli2018'
+  })
+
+emailService.sendMail('last update from nodeli', 'nodeli is growing even more!');
+
+let counter = 1;
+
+const startPolling = async () => {
+    try {
+        let bitfinex_prices = await bitfinex.fetchTicker('BTC/USD');
+        log.info(`--> REQUEST ${counter}: `, bitfinex_prices);
+    } catch(err) {
+        log.error(`--> REQUEST ${counter}: `, err);
+    }
+    counter++;
+};
+
+setInterval(startPolling, 300000);
+
+module.exports = exports = counter;
